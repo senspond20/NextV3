071f0e27de01c0e1d6f28e7150519184bdbfcb38
senspond20 <senspond@gmail.com>
2021-05-15 22:01:30 +0900

    init commit

---
+++ next.config.js
@@ -0,0 +1,49 @@
+require('dotenv').config();
+// const withTM = require("next-transpile-modules");
+// const withPlugins = require("next-compose-plugins");
+
+const withPlugins = require('next-compose-plugins')
+const sass = require('@zeit/next-sass')
+const css = require('@zeit/next-css')
+
+const nextConfig = {
+    webpack: function(config){
+        config.module.rules.push({
+            test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
+            use: {
+                loader: 'url-loader',
+                options: {
+                    limit: 100000,
+                    name: '[name].[ext]'
+                }}
+        })
+        return config
+    }
+}
+
+module.exports = withPlugins([
+    [css],
+    [sass, {
+        cssModules: true
+    }]
+], nextConfig)
+
+module.exports = {
+    output: {
+        globalObject: 'this'
+    },
+    env: {
+        VERSION : process.env.REACT_APP_SERVICE_VERSION,
+    }
+}
+
+// module.exports = withPlugins([
+//     [
+//         withTM,
+//         {
+//             transpileModules: [
+//                 "react-syntax-highlighter",
+//             ]
+//         }
+//     ],
+// ])
