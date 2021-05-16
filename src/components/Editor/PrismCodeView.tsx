3d3a1d5b8f8e647a7e0433488ed62e3958f1124f
senspond20 <senspond@gmail.com>
2021-05-17 02:58:25 +0900

    mark

--- src/components/Editor/PrismCodeView.tsx
+++ src/components/Editor/PrismCodeView.tsx
@@ -1,12 +1,14 @@
 import {Prism as SyntaxHighlighter} from "react-syntax-highlighter";
 import {dark} from "react-syntax-highlighter/dist/cjs/styles/prism";
+
 import React from "react";
 import ReactMarkdown, {CoreOptions} from "react-markdown";
 import gfm from "remark-gfm";
 import rehypeRaw from "rehype-raw";
+import {TransformOptions} from "react-markdown/src/ast-to-react";
 
 // @ts-ignore
-const PrismCodeView = ({node, inline, className, children, ...props}) =>{
+const PrismCodeView = ({node, inline, className, children, ...props} : TransformOptions)  =>{
     const match = /language-(\w+)/.exec(className || '')
 
     return !inline && match ? (
